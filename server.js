fdacc71548b7a3e63a4593a63e5015bd9c7a1981
Joao Lindgren <joao.lindgren@saiglobal.com>
2020-06-02 18:40:41 -0300

    Add Pug as production dependency and remove commented code.

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ app.get('/', async (req, res) => {
 
 app.use('/github', githubRoutes);
 
-/*
-app.get('/nofollowback/:username', async (req, res, next) => {
-    var results = await listGithub(req.params.username);
-    res.render('list', { title: 'Not Following You back list', results: results });
-});
-
-app.post('/nofollowback', async (req, res) => res.redirect(`/nofollowback/${req.body.username}`));
-*/
-
 app.listen(port, () => console.log(`Listening to port ${port}`));
 
 
